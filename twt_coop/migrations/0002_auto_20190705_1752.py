81d5a70d05505a8a36f281f74508bd0261678a9c
bambam2829 <yu118281028@yahoo.co.jp>
2019-08-31 20:27:39 +0900

    アプリ名変更

--- twt_coop/migrations/0002_auto_20190705_1752.py
+++ twt_coop/migrations/0002_auto_20190705_1752.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0001_initial'),
+        ('twt_coop', '0001_initial'),
     ]
 
     operations = [
